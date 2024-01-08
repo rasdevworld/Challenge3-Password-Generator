76d0d95e93b88129a8310dbd2a68fc82594c2754
Rasmi Roy <rasmiroy@yahoo.com>
2024-01-08 11:38:22 -0500

    updated the javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,47 +4,44 @@ var upperCase = lowerCase.toUpperCase();
 var numbers = "0123456789";
 var specialCharacters = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
 
-
-
 // Function to execute user input criteria
 function generatePassword() {
+
   var generatedPassword = "";
   var passwordLength = prompt("Enter the password length between 8 and 128");
+
+  // Check whether the password length meets the given criteria or not
   if(passwordLength >= 8 && passwordLength <= 128) {
+
     var confirmLowerCase = confirm("Do you want to include lowercase in the password?");
     var confirmUpperCase = confirm("Do you want to include uppercase in the password?");
     var confirmNumber = confirm("Do you want to include number in the password?");
     var confirmSpecialCharacter = confirm("Do you want to include special character in the password?");
 
-    // if the user confirms the password length
+    // if the user confirms the password length, then check each criteria using for loop
     for(var i=0; i < passwordLength; i++){
-      //console.log("passwordLength: "+i+":"+passwordLength)
-      //console.log("generatedPassword length: "+generatedPassword.length)
+
+      // Check whether the user confirms lowercase letter or not
       if(confirmLowerCase && generatedPassword.length < passwordLength) {
         var randomIndex = Math.floor(Math.random() * lowerCase.length);
-        //console.log(randomIndex);
         generatedPassword = generatedPassword + lowerCase[randomIndex];
-
       }
 
+      // Check whether the user confirms uppercase letter or not
       if(confirmUpperCase && generatedPassword.length < passwordLength) {
         var randomIndex = Math.floor(Math.random() * upperCase.length);
-        //console.log(randomIndex);
         generatedPassword = generatedPassword + upperCase[randomIndex];
       }
 
+      // Check whether the user confirms number or not
       if(confirmNumber && generatedPassword.length < passwordLength) {
         var randomIndex = Math.floor(Math.random() * numbers.length);
-        //console.log(randomIndex);
         generatedPassword = generatedPassword + numbers[randomIndex];
       }
-      //console.log("passwordLength "+passwordLength);
-      //console.log("generatedPassword "+generatedPassword);
 
+      // Check whether the user confirms special character or not
       if(confirmSpecialCharacter && generatedPassword.length < passwordLength) {
-        //console.log(specialCharacters);
         var randomIndex = Math.floor(Math.random() * specialCharacters.length);
-        //console.log(randomIndex);
         generatedPassword = generatedPassword + specialCharacters[randomIndex];
       }
     }
@@ -52,17 +49,12 @@ function generatePassword() {
           alert("Invalid entry, Password length must be between 8 and 128");
           return generatedPassword;
     }
-    
-   /* if(generatedPassword != "") {
-      return "Your generated password is: " + generatedPassword;
-    }else {
-      return generatedPassword;
-    }*/
+  
+    // Returns the output of the function
     return  "Your generated password is: " + generatedPassword;
-    //console.log(generatedPassword);
 }
 
-// Display the generated password in the password input field
+// Display the generated password 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
